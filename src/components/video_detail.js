3714afc9d3d11191f270e74a085ea9b045c173a2
RaulJimenezOrtega <hhkaos@gmail.com>
2017-05-03 00:42:55 +0200

    Removing console log

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -9,7 +9,6 @@ const VideoDetail = ({video}) => {
 
   const videoId = video.id.videoId;
   const url = `https://www.youtube.com/embed/${videoId}`;
-  console.log("url=",url)
 
   return (
       <div className="video-detail col-md-8">
